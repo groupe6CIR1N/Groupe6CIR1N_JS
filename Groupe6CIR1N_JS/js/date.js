a2e9ef5e2c38859d59bb6064d200119f997c46aa
groupe6CIR1N <122809747+groupe6CIR1N@users.noreply.github.com>
2023-05-10 12:25:21 +0000

    a

--- Groupe6CIR1N_JS/js/date.js
+++ Groupe6CIR1N_JS/js/date.js
@@ -1,8 +1,4 @@
 function Date(){
-    const options2 = {weekday: 'long',year:'numeric',month:'long',day:'numeric'};
-    let time = event2.toLocaleTimeString("en-FR");
-    context.beginPath();
-    context.font = "20px serif "
-    context.fillText(time,185,260);
-    context.stroke();
+    var now = new Date();
+    console.log(now);
 }
