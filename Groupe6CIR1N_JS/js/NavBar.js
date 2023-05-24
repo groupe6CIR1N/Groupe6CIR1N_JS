9345c36996f25c59143e416201075a3afa972e55
LERIOM <84094564+LERIOM@users.noreply.github.com>
2023-05-24 12:28:11 +0000

    a

--- Groupe6CIR1N_JS/js/NavBar.js
+++ Groupe6CIR1N_JS/js/NavBar.js
@@ -81,7 +81,9 @@ function detecterCopie() {
   document.addEventListener('copy', function(event) {
     event.preventDefault();
     alert('Attention ! Le plagiat est interdit en France. Veuillez respecter les droits d`auteur et citer vos sources.');
-  });
+    console.log("Attention ! Le plagiat est interdit en France. Veuillez respecter les droits d`auteur et citer vos sources.")
+});
+
 }
 
 window.onload = () => {
