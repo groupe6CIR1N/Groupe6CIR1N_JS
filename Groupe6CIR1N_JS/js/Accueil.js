d78763ae41d3162aa95143c3b2fa3162d03dc538
LERIOM <84094564+LERIOM@users.noreply.github.com>
2023-05-29 15:15:04 +0000

    a

--- Groupe6CIR1N_JS/js/Accueil.js
+++ Groupe6CIR1N_JS/js/Accueil.js
@@ -1,26 +1,28 @@
 function main(){
 
 }
-
+///////Zoom sur le schéma des compétences////////
 function zoom(){
     let diagramme = document.getElementsByClassName("diagramme")[0];
         diagramme.style.height = "600px";
         diagramme.style.width = "1200px";
 };
-
 function dezoom(){
     let diagramme = document.getElementsByClassName("diagramme")[0];
     diagramme.style.height = "400px";
     diagramme.style.width = "800px";
 };
 
- 
+//////////////////////////////////
+
+ //////Qui sommes nous/////////
+
 function afficherTexteMotParMot() {
   var texte = "Qui sommes nous ?";
   var div = document.querySelector(".QSN");
-  var mots = texte.split(" ");
+  var mots = texte.split(" ");// spare les mots 
   var indexMot = 0;
-
+// fonction qui affiche mot a mot 
   function afficherMotSuivant() {
     if (indexMot < mots.length) {
       div.innerHTML += mots[indexMot] + " ";
@@ -40,9 +42,9 @@ function afficherTexteMotParMot() {
 
   afficherMotSuivant();
 }
-
-
 afficherTexteMotParMot();
+
+///////////////////////////////
 
 main();
 
